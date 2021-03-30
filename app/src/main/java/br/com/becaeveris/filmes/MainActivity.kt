20975bc5f80c85027292d40341f0e3f87eb7905c
LuCabral <lucabral@everis.com>
2021-03-30 12:35:38 -0300

    Definindo o layout

--- app/src/main/java/br/com/becaeveris/filmes/MainActivity.kt
+++ app/src/main/java/br/com/becaeveris/filmes/MainActivity.kt
@@ -1,11 +1,17 @@
 package br.com.becaeveris.filmes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+// private lateinit var recycleMovies : RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       // recycleMovies = findViewById(R.id.recycler_movies)
     }
+
 }
--- app/src/main/java/br/com/becaeveris/filmes/model/Movie.kt
+++ app/src/main/java/br/com/becaeveris/filmes/model/Movie.kt
@@ -1,3 +1,18 @@
 package br.com.becaeveris.filmes.model
+//recebe os dados
+import com.google.gson.annotations.SerializedName
 
-data class Movie()
+    data class Movie(
+        @SerializedName("id")
+        val id:String,
+        @SerializedName("title")
+        val title:String,
+        @SerializedName("poster_path")
+        val poster_path:String) {
+    }
+
+
+
+
+
+
