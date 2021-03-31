bc7dd9f4daab5275cc20588589c03c20f86da1a9
LuCabral <lucabral@everis.com>
2021-03-31 03:08:12 -0300

    Configurando as permissões

--- app/src/main/java/br/com/becaeveris/movies/MainActivity.kt
+++ app/src/main/java/br/com/becaeveris/movies/MainActivity.kt
@@ -2,10 +2,14 @@ package br.com.becaeveris.movies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+     lateinit var recyclerMovies: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        recyclerMovies -findViewById(R.id.recycler_movies)
     }
 }
